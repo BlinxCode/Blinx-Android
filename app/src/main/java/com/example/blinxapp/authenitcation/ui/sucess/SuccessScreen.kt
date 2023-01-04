4e69248bbe3ffd66f859a1e65f51696c82bd7e3f
Bashorun Olajide John <bashorunolajide@gmail.com>
2023-01-04 21:52:24 +0100

    Added lotti animation to success screen

--- app/src/main/java/com/example/blinxapp/authenitcation/ui/sucess/SuccessScreen.kt
+++ app/src/main/java/com/example/blinxapp/authenitcation/ui/sucess/SuccessScreen.kt
@@ -6,13 +6,17 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.*
 import com.example.blinxapp.R
 import com.example.blinxapp.common.customviews.ContinueButtonButton
+import com.example.blinxapp.ui.theme.BlinxAppTheme
 import com.example.blinxapp.ui.theme.Typography
 import com.example.blinxapp.ui.theme.primaryGreen
 import com.example.blinxapp.ui.theme.secondaryGrey
@@ -26,10 +30,13 @@ fun SuccessScreen(
     val scrollState = rememberScrollState()
 
     Column(
-        modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.primary)
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primary)
             .padding(end = 20.dp, start = 20.dp)) {
 
         Column(
+            verticalArrangement = Arrangement.Center,
             modifier = Modifier
                 .verticalScroll(scrollState)
                 .weight(1f, false)
@@ -51,17 +58,27 @@ fun SuccessScreen(
             )
             //Description modifier
             Spacer(Modifier.size(16.dp))
-            Text(
-                text = "You have successfully created a blinx profile",
-                style = Typography.titleSmall,
-                textAlign = TextAlign.Start,
-                color = primaryGreen
-
-                )
+            SuccessAnimation()
             //Getting started button
             ContinueButtonButton(onProceedClicked, stringResource(R.string.proceed_login))
 
         }
+    }
+}
+
+@Composable
+fun SuccessAnimation() {
+    val composition by rememberLottieComposition(spec = LottieCompositionSpec.Url(
+        "https://lottie.host/10f73e19-5441-417e-aff3-b427591024a7/r49PIyMunV.json"))
+    Column(
+        modifier = Modifier
+            .padding(start=20.dp, top = 20.dp, bottom = 20.dp)
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.primary),
+        verticalArrangement = Arrangement.Center
+    ) {
+        LottieAnimation(composition =composition)
     }
 }
 
